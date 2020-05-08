745a03e10a5ed041ac985b842e17eea13fe6424d
aditap <aditap2018a@gmail.com>
2020-05-08 14:24:18 +0700

    update function follow old db structure

--- functions/index.js
+++ functions/index.js
@@ -54,8 +54,10 @@ exports.updateNearShopAndItem = functions.database.ref('/users/{userId}/location
             lon: location.Longitude
         }
         
-        var nearshoplist = new Array();
+        // array shop, item list
+        var t_nearshoplist = new Array();
         var t_nearitemlist = new Array();
+        var shopindex = 1;
 
         // find shop and item near user location
         admin.database().ref('/users').once('value')
@@ -71,21 +73,24 @@ exports.updateNearShopAndItem = functions.database.ref('/users/{userId}/location
                         var shopRef =  userId.child("shopinfo");
                         var shopInfo = shopRef.val();
                         var shopLocation = {
-                            lat: shopInfo.FixedLatitude,
-                            lon: shopInfo.FixedLongitude
+                            lat: shopInfo.latitude,
+                            lon: shopInfo.longitude
                         }
                         // find shop distance
                         var shopDistance = Distance.between(userLocation,shopLocation);
-                        if(shopDistance <= Distance('100 m'))
+                        if(shopDistance <= Distance('5 km'))
                         {
                             addshop =   {
+                                name: shopInfo.name,
                                 description: shopInfo.description,
-                                latitude: shopInfo.FixedLatitude,
-                                longitude: shopInfo.FixedLongitude,
-                                name: shopInfo.Name,
-                                phone: shopInfo.PhoneNumber
+                                latitude: shopInfo.latitude,
+                                longitude: shopInfo.longitude,
+                                phone: shopInfo.phone,
+                                lineid: shopInfo.lineid,
+                                facebookurl: shopInfo.facebookurl,
+                                website: shopInfo.website
                             }
-                            nearshoplist.push(addshop);
+                            t_nearshoplist.push(addshop);
                             
                             // add near by item
                             if(shopRef.child("itemlist").exists())
@@ -94,14 +99,17 @@ exports.updateNearShopAndItem = functions.database.ref('/users/{userId}/location
                                 shopitemRef.forEach(item => {
                                     var itemInfo = item.val();
                                     addItem = {
-                                        description: itemInfo.Description,
-                                        picURL: itemInfo.PicURL,
-                                        price: itemInfo.Price,
-                                        shopname: shopInfo.Name 
+                                        description: itemInfo.description,
+                                        picurl: itemInfo.picurl,
+                                        price: itemInfo.price,
+                                        shopname: shopInfo.name,
+                                        shopnumber: shopindex  
                                     }
                                     t_nearitemlist.push(addItem);
                                 });
                             }
+                            // update shop index
+                            shopindex += 1;
                         }
                     }       
                 }
@@ -110,25 +118,36 @@ exports.updateNearShopAndItem = functions.database.ref('/users/{userId}/location
             // write to firebase
             // near shop 
             const nearshopref = admin.database().ref('/users/' + userid + '/nearbyshop');
-            nearshopref.update({
-                shopinfo: nearshoplist,
-                totalshop: nearshoplist.length
+            nearshopref.set({
+                shopinfo: getnearbyshop(t_nearshoplist),
+                totalshop: t_nearshoplist.length
             });
             // near item
             const nearItemRef = admin.database().ref('/users/' + userid + '/nearbyitem');
-            nearItemRef.update({
+            nearItemRef.set({
                 iteminfo: getnearbyitem(t_nearitemlist),
                 totalitem: t_nearitemlist.length
             });
-            return console.log("Finish");   
+            return console.log("Update Finish : nearshop");   
         })
         .catch(error => {
             console.log(error);
         });
     });
 
+// convert array to object
+function getnearbyshop(t_nearshoplist) {
+    var nearshoplist = {};
+    var numShop = t_nearshoplist.length;
+    var iShop;
+    for(iShop = 1;iShop <= numShop;iShop++) {
+        nearshoplist["shop" + iShop.toString()] = t_nearshoplist[iShop - 1];
+    }
+    return nearshoplist;
+}
+// convert array to object 
 function getnearbyitem(t_nearitemlist) {
-    var nearitemlist = [];
+    var nearitemlist = {};
     var numItem = t_nearitemlist.length;
     console.log("numItem " + numItem);
     var numPage = parseInt(numItem/10);
@@ -140,18 +159,18 @@ function getnearbyitem(t_nearitemlist) {
         var itemlist = [];
         if(iPage !== numPage){
             for(iItem = 0;iItem < 10;iItem++){
-                itemlist.push(t_nearitemlist[eachItem])
+                itemlist["item" + (iItem + 1).toString()] = t_nearitemlist[eachItem];
                 eachItem += 1;
             }
-            nearitemlist.push(itemlist);
+            nearitemlist["page" + (iPage + 1).toString()] = itemlist;
         }
         else
         {
             for(iItem = 0;iItem < remainItem;iItem++) {
-                itemlist.push(t_nearitemlist[eachItem])
+                itemlist["item" + (iItem + 1).toString()] = t_nearitemlist[eachItem]
                 eachItem += 1;
             }
-            nearitemlist.push(itemlist);
+            nearitemlist["page" + (iPage + 1).toString()] = itemlist;
         }
         
     }
